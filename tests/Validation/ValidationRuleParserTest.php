510f50d6e9b97a3ba8a49d37f7defe0625f248d2
Steve Bauman <steven_bauman@outlook.com>
2023-01-06 17:30:30 -0500

    Add tests for parsing multiple regex rules in a single rule string

--- tests/Validation/ValidationRuleParserTest.php
+++ tests/Validation/ValidationRuleParserTest.php
@@ -131,7 +131,7 @@ class ValidationRuleParserTest extends TestCase
         $this->assertSame('regex:/^(bar)$/i', $exploded->rules['items.0.type'][1]);
     }
 
-    public function testExplodeFailsParsingRegexWithOtherRulesInSingleString()
+    public function testExplodeProperlyParsesRegexWithOtherRulesInSingleString()
     {
         $data = ['items' => [['type' => 'foo']]];
 
@@ -140,8 +140,22 @@ class ValidationRuleParserTest extends TestCase
         );
 
         $this->assertSame('in:foo', $exploded->rules['items.0.type'][0]);
-        $this->assertSame('regex:/^(foo', $exploded->rules['items.0.type'][1]);
-        $this->assertSame('bar)$/i', $exploded->rules['items.0.type'][2]);
+        $this->assertSame('regex:/^(foo|bar)$/i', $exploded->rules['items.0.type'][1]);
+    }
+
+    public function testExplodeProperlyParsesMultipleRegexRulesWithOtherRulesInSingleString()
+    {
+        $data = ['items' => [['type' => 'foo']]];
+
+        $exploded = (new ValidationRuleParser($data))->explode(
+            ['items.*.type' => 'in:foo,bar|notregrex:/^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]*$/i|max:20|not_Regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]*$/|REGEX:/^(foo|bar)$/imsxADSUXJu']
+        );
+
+        $this->assertSame('in:foo,bar', $exploded->rules['items.0.type'][0]);
+        $this->assertSame('notregrex:/^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]*$/i', $exploded->rules['items.0.type'][1]);
+        $this->assertSame('max:20', $exploded->rules['items.0.type'][2]);
+        $this->assertSame('not_Regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]*$/', $exploded->rules['items.0.type'][3]);
+        $this->assertSame('REGEX:/^(foo|bar)$/imsxADSUXJu', $exploded->rules['items.0.type'][4]);
     }
 
     public function testExplodeGeneratesNestedRules()
